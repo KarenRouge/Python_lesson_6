8493a2036e3745e52f2338f5464b5ab5d27cda89
KarenRouge <karenrouge1991@gmail.com>
2021-11-29 20:22:46 -0500

    Task_1 complete

--- Task_1.py
+++ Task_1.py
@@ -1,19 +1,19 @@
+from time import sleep
 class TrafficLight:
-    color = "red"
+    _color = ['red', 'yellow', 'green']
 
     def running(self):
-        print(self)
-        import time
-        time.sleep(7)
-        _color = "yellow"
-        print(self)
-        time.sleep(2)
-        _color = "green"
-        print(self)
-        time.sleep(3)
-        print("Stop")
+        i = 0
+        while i < 3:
+            print(TrafficLight._color[i])
+            if i == 0:
+                sleep(7)
+            if i == 1:
+                sleep(3)
+            if i == 2:
+                sleep(7)
+            i = i + 1
         return
 
-
-example = TrafficLight
-print(example.color)
+example = TrafficLight()
+example.running()
